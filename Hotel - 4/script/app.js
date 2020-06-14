db8aecbd5e31caf7fe43e68310d976fe424a35d5
paulzay <paulotieno2@gmail.com>
2020-06-14 07:03:34 +0300

    add navbar and home slider section

---
+++ Hotel - 4/script/app.js
@@ -0,0 +1,5 @@
+const hum = document.getElementById("humberger");
+hum.addEventListener("click", function (e) {
+  const xmMenu = document.getElementById("xmMenu");
+  xmMenu.classList.toggle("hide");
+});
